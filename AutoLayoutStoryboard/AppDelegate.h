623f0a86fed95a54c31b6b1793bb4bee6dcc941f
J.K <K_940201@hotmail.com>
2017-08-01 15:07:21 -0700

    Initial Commit

---
+++ AutoLayoutStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryboard
+//
+//  Created by Jaewon Kim on 2017-08-01.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
